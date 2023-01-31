746dd6880b151fc6b34d8763d17af374d4d64766
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-01-31 22:05:41 +0100

    add ngfor a siderbar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -13,7 +13,9 @@ export class BusquedaComponent implements OnInit {
     // document.querySelector('input')?.value = ''
     
    const valor= this.txtBuscar.nativeElement.value
-  
+  if(valor.trim().length===0){
+    return;
+  }
    this.gifsService.buscarGifs(valor)
    this.txtBuscar.nativeElement.value ='';
   }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,12 +7,18 @@ export class GifsService {
    
   private _historial: string[]=[];
   get historial(){
+
     return [...this._historial];
   }
 
   buscarGifs(query: string){
-    this._historial.unshift(query);
+   
+    query = query.trim().toLocaleLowerCase();
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+    }
     console.log(this._historial)
+    this._historial = this._historial.splice(0,10);
   }
   constructor() { }
 }
